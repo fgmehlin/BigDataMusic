5ed98e22a89c42474b88fdff985ed7f29bd75c4d
fgmehlin <floran.gmehlin@gmail.com>
2014-10-19 03:15:38 +0200

    added console prints

--- Music/src/main/java/BigData/Music/App.java
+++ Music/src/main/java/BigData/Music/App.java
@@ -1,13 +1,8 @@
 package BigData.Music;
 
 import java.io.File;
-import java.util.Enumeration;
-
-import javax.swing.tree.DefaultMutableTreeNode;
 
 import ncsa.hdf.hdf5lib.HDF5Constants;
-import ncsa.hdf.object.FileFormat;
-import ncsa.hdf.object.HObject;
 import ncsa.hdf.object.h5.H5File;
 
 /**
@@ -17,70 +12,83 @@ import ncsa.hdf.object.h5.H5File;
 public class App {
 	private static H5Handler h5h;
 	private static int cntFiles = 0;
-	
+
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		h5h = H5Handler.getInstance();
-		readFromH5();
+		readFromH5("Johnny Cash", "I Got Stripes (Live)");
 	}
 
-	public static void readFromH5() {
+	public static void readFromH5(String artist, String track_name) {
 
-		String fileNameFromTrack = h5h.queryTrackFile("Elliot Smith", "Angeles");
-		File[] files = new File("/Volumes/Data/ETHZ/Big Data/MillionSongSubset/data/").listFiles();
-	    File foundFile = getFile(files, fileNameFromTrack);
-	    
-	    System.out.println("nb files scanned : " + cntFiles);
-	    //H5File file2 = new H5File2(foundFile.)
-		if(foundFile != null)
+		String fileNameFromTrack = h5h.queryTrackFile(artist, track_name);
+		File[] files = new File(
+				"/Volumes/Data/ETHZ/Big Data/MillionSongSubset/data/")
+				.listFiles();
+		File foundFile = getFile(files, fileNameFromTrack);
+
+		System.out.println("nb files scanned : " + cntFiles);
+		// H5File file2 = new H5File2(foundFile.)
+		if (foundFile != null)
 			System.out.println(foundFile.getAbsolutePath());
 		else
 			System.out.println("404: File not found");
-		
-		H5File file = new H5File(
-				foundFile.getAbsolutePath(),
+
+		H5File file = new H5File(foundFile.getAbsolutePath(),
 				HDF5Constants.H5F_ACC_RDONLY);
 		try {
-			// open file and retrieve the file structure
-			file.open();
-			System.out.println(file.getName());
-			System.out.println(file.getNumberOfMembers());
-
-			System.out.println(hdf5_getters.get_artist_name(file));		
-			
-			
-			
-			file.close();
-
-		} catch (Exception ex) {
-			System.out.println(ex);
+			System.out.println("artist id : "
+					+ hdf5_getters.get_artist_id(file));
+			System.out.println("artist name : "
+					+ hdf5_getters.get_artist_name(file));
+			System.out.println("song title : " + hdf5_getters.get_title(file));
+			System.out.println("artist familiarity : "
+					+ hdf5_getters.get_artist_familiarity(file));
+			System.out.println("artist hotttness : "
+					+ hdf5_getters.get_artist_hotttnesss(file));
+			System.out.println("artist location : "
+					+ hdf5_getters.get_artist_location(file));
+			System.out.println("song danceability : "
+					+ hdf5_getters.get_danceability(file));
+			System.out.println("song duration : "
+					+ hdf5_getters.get_duration(file));
+			System.out
+					.println("song energy : " + hdf5_getters.get_energy(file));
+			System.out.println("song key : " + hdf5_getters.get_key(file));
+			System.out.println("song loudness : "
+					+ hdf5_getters.get_loudness(file));
+			System.out.println("song mode : " + hdf5_getters.get_mode(file));
+			System.out.println("song hotttness : "
+					+ hdf5_getters.get_song_hotttnesss(file));
+			System.out.println("song tempo : " + hdf5_getters.get_tempo(file));
+			System.out.println("song year : " + hdf5_getters.get_year(file));
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-
 	}
-	
-	
-	private static File getFile(File[] files, String wantedFile){
+
+	private static File getFile(File[] files, String wantedFile) {
 		File foundFile = null;
-		for(File file : files){
+		for (File file : files) {
 			cntFiles++;
-			if(foundFile != null)
+			if (foundFile != null)
 				break;
-			if(file.isDirectory()){
+			if (file.isDirectory()) {
 				foundFile = getFile(file.listFiles(), wantedFile);
-				if(foundFile != null)
+				if (foundFile != null)
 					System.out.println("foundfile = " + foundFile.getName());
 				else
 					System.out.println("foundFile is null");
-			}else if(file.getName().equals(wantedFile+".h5")){
-			 	System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> FOUND <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
+			} else if (file.getName().equals(wantedFile + ".h5")) {
+				System.out
+						.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> FOUND <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
 				foundFile = file;
 				break;
-			}else{}
+			} else {
+			}
 		}
 		return foundFile;
 	}
-	
-
-
 
 }
